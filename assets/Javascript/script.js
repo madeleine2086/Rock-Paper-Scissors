68c1842ba6a68f58cf625949f5e4e03761a2d4a8
Magda Dmitruk <dmitrukm@gmail.com>
2023-10-08 18:03:32 +0000

    "Create basic function structure"

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -3,3 +3,22 @@
 const yourChoice = document.getElementById("yourChoice");
 const computerChoice = document.getElementById("computerChoice");
 const seeResult = document.getElementById("result");
+const buttons = document.getElementsByTagName("button");
+let player;
+let computer;
+let result;
+
+for (let button of buttons) {
+    button.addEventListener("click", () => {
+        player = button.textContent;
+        computerPick();
+    })
+}
+
+function computerPick() {
+
+}
+
+function checkResult() {
+    
+}
