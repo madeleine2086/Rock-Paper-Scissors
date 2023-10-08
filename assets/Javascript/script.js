de0f01cf09be54b361fc67daf3b872bd7c257951
Magda Dmitruk <dmitrukm@gmail.com>
2023-10-08 17:32:00 +0000

    "Create variables in script.js"

---
+++ assets/Javascript/script.js
@@ -0,0 +1,5 @@
+//Variables
+
+const yourChoice = document.getElementById("yourChoice");
+const computerChoice = document.getElementById("computerChoice");
+const seeResult = document.getElementById("result");
